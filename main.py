3b271badd53b5e50905e8c5e4265a7c73cb6e94d
Mark Chafin <65318063+mjcdk9@users.noreply.github.com>
2023-05-03 11:08:50 -0500

    Added files via upload
    Added existing files to continue project in GitHub.

---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+import pandas as pd
+import PySimpleGUI as sg
+from main_psg import *
+import logging
+import threading
+import time
+
+
+
+def split_csv_file(input_file, output_folder, rows_per_file):
+    # Load the input CSV file into a pandas dataframe
+    df = pd.read_csv(input_file, encoding='latin-1')
+
+    status = "Loading..."
+
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Calculate the number of output files needed
+    num_output_files = len(df) // rows_per_file + 1
+
+    # Split the data and save to output files
+    for i in range(num_output_files):
+        start_index = (i * rows_per_file) + 1
+        end_index = min((i + 1) * rows_per_file, len(df))
+        output_file = os.path.join(output_folder, f"output_{i}.csv")
+        df_1 = df.iloc[:1]
+        frames = [df_1, df]
+        df = pd.concat(frames)
+        df[start_index:end_index].to_csv(output_file, index=False)
+
+    status = "Complete"
+
+
+def extract_top_5(input_file_path, output_folder, number_of_rows_in_short_file):
+    # Load the input CSV file into a pandas dataframe
+    df = pd.read_csv(input_file, encoding='latin-1')
+
+    status = "Loading..."
+
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Split the data and save to output files
+    start_index = (1)
+    end_index = (number_of_rows_in_short_file + 1)
+    output_short_file = os.path.join(output_folder, "short.csv")
+    df_1 = df.iloc[:1]
+    frames = [df_1, df]
+    df = pd.concat(frames)
+    df[start_index:end_index].to_csv(output_short_file, index=False)
+
+    # Trim top X rows off large file and save
+    rest_of_df = df.drop(range(number_of_rows_in_short_file))
+    output_rest_of_file = os.path.join(output_folder, "output_rest_of_file.csv")
+    rest_of_df.to_csv(output_rest_of_file, index=False)
+
+    status = "Complete"
+
+
+input_file = "file.csv"
+output_folder = "output"
+rows_per_file = 50000
+status = "testing"
+
+# split_csv_file(input_file, output_folder, rows_per_file)
+# extract_top_5(input_file, output_folder, 3)
+
+
---
+++ main_psg.py
@@ -0,0 +1,85 @@
+# img_viewer.py
+import time
+
+import PySimpleGUI as sg
+import os.path
+
+from main import *
+# from main import extract_top_5, input_file, output_folder, split_csv_file, status
+
+
+
+def thread_function(name):
+    print("thread start")
+    logging.info("Thread %s: starting", name)
+    extract_top_5(values["-PATH-"], output_folder, 5)
+    print("thread stop")
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+
+
+
+
+
+
+
+
+
+# First the window layout in 2 columns
+
+file_list_column = [
+        [sg.Text("File "),
+        # sg.In(size=(25, 1), enable_events=True, key="-FILE-"),
+        sg.Input(key="-PATH-", change_submits=True),
+        sg.FileBrowse(key="-FILE-", change_submits=True)],
+        [sg.Text(status, key="-STATUS-")],
+]
+
+# For now will only show the name of the file that was chosen
+image_viewer_column = [
+    [sg.Text("Choose an option below:")],
+    [sg.Text(size=(40, 1), key="-TOUT-")],
+    [sg.Button("Short File Splitter", key="-HEADSPLIT-"),
+    sg.Button("Large file splitter", key="-MULTISPLIT-")],
+    # [sg.Input(key="-Number of rows per file-")],
+
+]
+
+# ----- Full layout -----
+layout = [
+    [
+        sg.Column(file_list_column),
+        sg.VSeperator(),
+        sg.Column(image_viewer_column),
+    ]
+]
+
+window = sg.Window("File Browser", layout)
+
+
+while True:
+    event, values = window.read()
+    # print(values["-Path-"])
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+    elif event == "-HEADSPLIT-":
+        status = "Loading..."
+        window["-STATUS-"].update(status)
+        event == "-HEADSPLIT-"
+        print(values["-PATH-"])
+        print("headsplit")
+        x = threading.Thread(target=thread_function, args=(1,))
+        x.start()
+        # extract_top_5(values["-PATH-"], output_folder, 5)
+        status = "Complete"
+        window["-STATUS-"].update(status)
+
+    elif event == "-MULTISPLIT-":
+        status = "Loading..."
+        window["-STATUS-"].update(status)
+        print(values["-PATH-"])
+        print("multisplit")
+        split_csv_file(values["-PATH-"], output_folder, 50)
+        status = "Complete"
+        window["-STATUS-"].update(status)
